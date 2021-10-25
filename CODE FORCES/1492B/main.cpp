742f448bfb5fc760bb56543fee397b552e14c70c
Fady Kamal <60224155+Fady-kamal@users.noreply.github.com>
2021-10-25 11:14:04 +0200

    Update main.cpp

--- CODE FORCES/1492B/main.cpp
+++ CODE FORCES/1492B/main.cpp
@@ -3,71 +3,6 @@ using namespace std;
 using ll = long long;
 
 
-int getIndex(vector<int> vec, int element, char direction) {
-
-	/*
-	*
-	* Usage:
-	*  #get the first index of the given element
-	*
-	* Arguments:
-	*  #vec: the vector searched for the index of a given element
-	*  #element: the number that we want to know its index
-	*  #direction: search from left to right "L" or from right to left "R"
-	*
-	* return:
-	*  #index: the index of the element. It returns -1 if the element
-	*          is not int the vec
-	*
-	*/
-
-	int index = -1;
-
-
-	if (direction == 'L') {
-
-		for (int i = 0; i < vec.size(); i++) {
-
-			if (element == vec[i]) {
-				index = i;
-				break;
-			}
-
-		}
-
-	}
-
-	else if (direction == 'R') {
-
-		for (int i = vec.size() - 1; i >= 0; i--) {
-
-			if (element == vec[i]) {
-				index = i;
-				break;
-			}
-
-		}
-
-	}
-	return index;
-}
-
-
-
-//void printVector(vector<int>& vec) {
-//
-//	cout << '[';
-//	for (int i = 0; i < vec.size(); i++) {
-//
-//		if (i > 0)
-//			cout << ", ";
-//		cout << vec[i];
-//	}
-//
-//	cout << ']';
-//
-//}
-
 int main()
 {
 
@@ -90,20 +25,20 @@ int main()
 
 
 		int start = 0;
-		int end = n;
+		int current_max = n;
 
 		for (int i = n; i > 0; i--) {
 
 			start = position[i]; //index of element i 
 
-			if (end> start) {
+			if (current_max > start) {
 
-				for (int j = start; j < end; j++) {
+				for (int j = start; j < current_max; j++) {
 					cout << p[j] << " ";
 				}
 
 
-				end = start;
+				current_max = start;
 			}
 		}
 
