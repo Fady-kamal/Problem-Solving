b7b3c4b559552052e2e1581861474edf917d8fc6
Fady Kamal <60224155+Fady-kamal@users.noreply.github.com>
2021-11-05 10:55:21 +0200

    Add files via upload

---
+++ CODE FORCES/1234D/main.cpp
@@ -0,0 +1,98 @@
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define Pair pair<ll,ll>
+
+using namespace std;
+#define fr(n) for(int i=0;i<n;i++)
+#define frj(n) for(int j=0;j<n;j++)
+#define frk(n) for(int k=0;k<n;k++)
+#define fr1(n) for(int i=1;i<=n;i++)
+#define el '\n'
+#define f first 
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define allrev(x) (x).rbegin(), (x).rend()
+
+
+int main() {
+
+
+ 
+	string s;
+	cin >> s;
+	int q;
+	cin >> q;
+	vector<set<int>> pos(26);
+
+
+	//hold the possible positions for every letter 
+	for (int i = 0; i < s.size(); ++i) {
+
+		pos[s[i] - 'a'].insert(i);
+	}
+
+
+	while (q--) {
+
+		int x;
+
+		cin >> x;
+
+		if (x == 1) {
+			int idx;
+			cin >> idx;
+			char c;
+			cin >> c;
+
+			--idx;
+
+			
+
+
+			//remove the element at idx c
+			pos[s[idx] - 'a'].erase(idx);
+			
+
+			//set
+			s[idx] = c;
+
+			//add
+			pos[c - 'a'].insert(idx);
+		}
+
+
+
+	 
+		// x= 2
+		else {
+
+			int cnt = 0;
+			int l, r;
+			cin >> l >> r;
+			--l, --r;
+
+
+			for (int i = 0; i < 26; ++i) {
+
+				//to  be in the range[l,r]
+				//the idx shoud be satisfy that l<=idx<=r
+				auto it = pos[i].lower_bound(l);
+				if (it != pos[i].end() && *it <= r)
+					cnt++;
+			}
+
+			cout << cnt << el;
+		}
+
+		
+
+
+	}
+
+}
+
+
+
+
