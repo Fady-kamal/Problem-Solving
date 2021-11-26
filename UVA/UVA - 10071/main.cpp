55bb201c4fd0f15702abeeb37b17e178f0d44ff3
Fady Kamal <60224155+Fady-kamal@users.noreply.github.com>
2021-11-26 23:29:29 +0200

    Add files via upload

---
+++ UVA/UVA - 10071/main.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define Pair pair<ll,ll>
+
+using namespace std;
+#define fr(n) for(int i=0;i<n;i++)
+#define frj(n) for(int j=0;j<n;j++)
+#define frk(n) for(int k=0;k<n;k++)
+#define fr1(n) for(int i=1;i<=n;i++)
+#define el '\n'
+#define f first 
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define allrev(x) (x).rbegin(), (x).rend()
+#define fast  ios::sync_with_stdio(0), cin.tie(0) , cout.tie(0);
+//cout << fixed << setpercision(# of digits after the fixed point 
+
+
+bool cmp(int a, int b) {
+
+	return a > b;
+}
+
+int main() {
+
+	int v, t;
+	while (cin >> v >> t) {
+		cout << 2 * v * t << el; 
+	}
+	cout << " heloo world";
+}
+
