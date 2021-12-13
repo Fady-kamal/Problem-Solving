42d3293ce3f4b71eee5a5251940dc63c00042f3c
Fady Kamal <60224155+Fady-kamal@users.noreply.github.com>
2021-12-13 09:59:13 +0200

    Add files via upload

---
+++ CODE FORCES/770A/main.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define Pair pair<ll,ll>
+
+using namespace std;
+#define fr(n) for(int i=0;i<n;i++)
+#define frj(n) for(int j=0;j<n;j++)
+#define frk(n) for(int k=0;k<n;k++)
+#define fr1(n) for(int i=1;i<=n;i++)
+#define el '\n'
+#define f first 
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define allrev(x) (x).rbegin(), (x).rend()
+#define fast  ios::sync_with_stdio(0), cin.tie(0) , cout.tie(0);
+//cout << fixed << setpercision(# of digits after the fixed point 
+
+const int N = 3e5 + 5;
+
+bool cmp(int a, int b) {
+
+	return a > b;
+}
+
+
+
+
+int n, Case = 0, ans[20] = { 1 };
+
+bool isPrime(int n) {
+
+
+	if (n <= 1)
+		return false;
+	else {
+		for (int i = 2; i <= sqrt(n); i++) {
+			if (n % i == 0)
+				return false;
+		}
+	}
+
+	return true;
+}
+
+
+
+
+
+
+
+
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	string alpha = "abcdefghijklmnopqrstuvwxyz";
+	string str = "";
+	fr(n) {
+		str += alpha[i % k];
+ 	}
+
+	fr(str.size()) {
+		cout << str[i];
+	}
+ 
+ 
+}
